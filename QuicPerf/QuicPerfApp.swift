9d7c0c84205206ab767f194950160b94b3374899
Thad House <thadhouse1@gmail.com>
2022-03-21 12:57:32 -0700

    Initial Commit

---
+++ QuicPerf/QuicPerfApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuicPerfApp.swift
+//  QuicPerf
+//
+//  Created by Thad House on 3/21/22.
+//
+
+import SwiftUI
+
+@main
+struct QuicPerfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
